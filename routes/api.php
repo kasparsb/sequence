1e982d1bd7735afb05a5b01b99593e293387340b
Kaspars Bulins <kasparsb@hotmail.com>
2022-05-06 18:44:16 +0300

    Api routes

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\NumberFormatsController;
+
 Route::middleware('auth:api')->group(function () {
 
-    Route::get('/number/formats', 'Api\NumberFormatsController@index');
+    Route::get('/number/formats', [NumberFormatsController::class, 'index']);
 
-    Route::get('/number/generate/{numberFormatId}/{count?}', 'Api\NumberFormatsController@generate');
+    Route::get('/number/generate/{numberFormatId}/{count?}', [NumberFormatsController::class, 'generate']);
 });
